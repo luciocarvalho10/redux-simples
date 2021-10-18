7e633a0d9738ca1558d775b9a9f23e13c0188e80
Lucio <luciocarvalho10@hotmail.com.br>
2021-10-18 01:20:13 -0400

    criando e usando componente Intervalo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import Intervalo from './components/Intervalo';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Card title='Card 1' Red>X</Card>
-        <Card title='Card 2' Green>X</Card>
+        <Intervalo>X</Intervalo>
       </div>
       <div className="linha">
+        <Card title='Card 2' Green>X</Card>
         <Card title="Card 3" Blue>Y</Card>
         <Card title="Card 4" Purple>Y</Card>
       </div>
